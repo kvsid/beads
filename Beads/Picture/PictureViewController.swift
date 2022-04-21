484768e34f6d0842ecd7bebb5cff1b3144e5888d
antnruban <antnruban@gmail.com>
2022-04-21 15:54:27 +0300

    Adjust 'CustomCell' and small clean up.

--- Beads/Picture/PictureViewController.swift
+++ Beads/Picture/PictureViewController.swift
@@ -18,57 +18,31 @@ class PictureViewController: UIViewController {
 
         schemeCollectionView.delegate = self
         schemeCollectionView.dataSource = self
-
         schemeCollectionView.register(UINib(nibName: "CustomCell", bundle: nil), forCellWithReuseIdentifier: "CustomCell")
 
     }
-
-
 }
 
 extension PictureViewController: UICollectionViewDelegate, UICollectionViewDataSource {
 
    func numberOfSections(in collectionView: UICollectionView) -> Int {
-       let height = Int(UserDefaults().height ?? "1")
-       return height!
-        }
+     if let height = Int(UserDefaults().height ?? "4") {
+       return height } else { return 4 }
+  }
 
 
    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-       let width = Int(UserDefaults().width ?? "1")
-       return width!
-        }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CustomCell
-
-            return cell
+     if let width = Int(UserDefaults().width ?? "4") {
+       return width } else { return 4 }
     }
 
-//    func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CustomCell
-//        cell.backgroundColor = UIColor.red
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CustomCell
-//        cell.backgroundColor = UIColor.cyan
-//    }
-
-//    private func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! CustomCell
-//        cell.backgroundColor = UIColor.red
-//    }
-//
-//    private func collectionView(_ collectionView: UICollectionView, didDeselectItemAtIndexPath indexPath: NSIndexPath) {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! CustomCell
-//        cell.backgroundColor = UIColor.cyan
-//    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CustomCell
+      return cell
+    }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! CustomCell
-//        cell.cellView.layer.borderColor = UIColor.red.cgColor
-        cell.backgroundColor = .red
-//        cell.configure()
+      let cell = collectionView.cellForItem(at: indexPath) as! CustomCell
+        cell.configure()
     }
 }
